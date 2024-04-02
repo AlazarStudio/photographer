c2e20715a1972b92a6d7abf4d6afa5a5d2dd44e0
Ameron666 <aisakebi69@gmail.com>
2024-04-02 19:31:56 +0300

    feedback done

--- mail/feedback.php
+++ mail/feedback.php
@@ -1,36 +1,31 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-    $name = $_POST['name'];
+    $name = $_POST['uname'];
     $phone = $_POST['phone'];
     $email = $_POST['email'];
-    $date = $_POST['date'];
-    $time = $_POST['time'];
-    $hour = $_POST['hour'];
+    $msg = $_POST['msg'];
 
     if (
         $name != "" &&
         $phone != "" &&
         $email != "" &&
-        $date != "" &&
-        $time != "" &&
-        $hour != ""
+        $msg != "" 
     ) {
-        $subject = "Заказ от $name";
+        $subject = "Сообщение от $name";
 
         $message = "";
         $message .= "Имя: $name\n";
         $message .= "Телефон: $phone\n";
         $message .= "Email: $email\n";
-        $message .= "date $date\n";
-        $message .= "time $time\n";
-        $message .= "hour $hour\n";
+        $message .= "Сообщение: $msg"; 
 
         $to = "8rusik8@mail.ru, info@alazarstudio.ru";
         $headers = "From: $email";
 
         if (mail($to, $subject, $message, $headers)) {
-            echo "Ваш заказ успешно оформлен.";
+            
+            header("Location: /learning.html");
         } else {
             echo "При отправке заказа произошла ошибка.";
         }
